3a18f542e6c62ba58678f299a9adb56a47486757
Lu, Kenny <kenny.lu@intel.com>
2023-07-04 17:27:24 -0700

    Add /dist to git tracking

---
+++ dist/utils/copyPasting.d.ts
@@ -0,0 +1,4 @@
+export declare const parseTextHtmlData: (data: string) => string[][];
+export declare const parseTextPlainData: (data: string) => string[][];
+export declare const encodeHtml: (str: string) => string;
+//# sourceMappingURL=copyPasting.d.ts.map
